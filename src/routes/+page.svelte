fe1ecaad9e0f7d0fd5ca55b03695276a84e0b7f1
Paulius Andriekus <paulando@paulando.lan>
2024-02-19 12:28:51 +0200

    cl editor inside dialog element

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button class="btn" on:click={() => my_modal_1.showModal()}>show daisyui modal</button>
+<dialog bind:this={my_modal_1} id="my_modal_1" class="modal">
+  <div class="modal-box">
+    {@html html}
+    <Editor {html} on:change={(evt)=>html = evt.detail}/>
+
+    <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+
+
+<button on:click={() => (showModal = true)}> show svelte modal </button>
+<Modal bind:showModal>
+    {@html html}
+    <Editor {html} on:change={(evt)=>html = evt.detail}/>
+</Modal>
+
+
+{@html html}
+<Editor {html} on:change={(evt)=>html = evt.detail}/>
+
+
+<script lang="ts">
+    import Modal from "$lib/Modal.svelte";
+
+    let my_modal_1: HTMLDialogElement;
+    let showModal = false;
+
+    import Editor from "cl-editor/src/Editor.svelte"
+
+    let html = '<div>try bold any word multiple times</div><p>try bold this paragraph multiple times</p>'
+    let editor
+</script>
